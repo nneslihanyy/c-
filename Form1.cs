fa682a1d3b66d83b6cf2ec768741f0ab3b67de0e
Neslihan Yağmurca <95581001+nneslihanyy@users.noreply.github.com>
2023-01-06 18:29:02 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client bilgiGetir = new ServiceReference1.Service1Client();
+            int sonuc = bilgiGetir.personelEkle("Ayse ", "Yilmaz", 2);
+            if (sonuc == 0)
+            {
+                MessageBox.Show("Personel Eklendi");
+            }
+            else
+            {
+                MessageBox.Show("Ekleme islemi gerceklestirilemedi");
+            }
+        }
+    }
+}
